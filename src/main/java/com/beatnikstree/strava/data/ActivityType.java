4324de7771710dd7a45d997187f236dd95a7a276
Carlton Hanna <carlton@graymatterssoftware.com>
2017-01-09 07:28:23 -0700

    Add ActivityType Enum

---
+++ src/main/java/com/beatnikstree/strava/data/ActivityType.java
@@ -0,0 +1,52 @@
+package com.beatnikstree.strava.data;
+
+/**
+ * Created by nullpointer0x00 on 1/2/17.
+ */
+public enum ActivityType {
+
+    RIDE("Ride"),
+    RUN("Run"),
+    SWIM("Swim"),
+    HIKE("Hike"),
+    WALK("Walk"),
+    ALPINE_SKI("AlpineSki"),
+    BACKCOUNTRY_SKI("BackcountrySki"),
+    CANOEING("Canoeing"),
+    CROSSFIT("Crossfit"),
+    E_BIKE_RIDE("EBikeRide"),
+    ELLIPTICAL("Elliptical"),
+    ICE_SKATE("IceSkate"),
+    INLINE_SKATE("InlineSkate"),
+    KAYAKING("Kayaking"),
+    YOGA("Yoga"),
+    KITESURF("Kitesurf"),
+    NORDIC_SKI("NordicSki"),
+    ROCK_CLIMBING("RockClimbing"),
+    ROLLER_SKI("RollerSki"),
+    ROWING("Rowing"),
+    SNOWBOARD("Snowboard"),
+    SNOWSHOE("Snowshoe"),
+    STAIR_STEPPER("StairStepper"),
+    STAND_UP_PADDLING("StandUpPaddling"),
+    SURFING("Surfing"),
+    VIRTUAL_RIDE("VirtualRide"),
+    WEIGHT_TRAINING("WeightTraining"),
+    WINDSURF("Windsurf"),
+    WORKOUT("Workout");
+
+    private String value;
+
+    ActivityType(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
+}
